c1836ee4c0b3aa26da606a22aec79ac014a37b00
Daniela Romero <daniela.romero@epn.edu.ec>
2023-01-15 19:56:33 -0500

    Intent Explicito e Implicito

--- 01-android/app/src/main/java/com/example/darmapplication/CIntentExplicitParametros.kt
+++ 01-android/app/src/main/java/com/example/darmapplication/CIntentExplicitParametros.kt
@@ -1,11 +1,30 @@
 package com.example.darmapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CIntentExplicitParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cintent_explicit_parametros)
+
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton
+            .setOnClickListener { devolverRespuesta() }
+    }
+    fun devolverRespuesta(){
+        val intentDevolverParametros = Intent()
+        intentDevolverParametros.putExtra("nombreModificaado", "Vicente")
+        intentDevolverParametros.putExtra("edadModificado",33)
+        setResult(
+            RESULT_OK,
+            intentDevolverParametros
+        )
+        finish()
     }
 }
---
+++ 01-android/app/src/main/java/com/example/darmapplication/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.darmapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+
+    val contenidoIntentExplicito =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    val data = result.data
+                    Log.i("intent-epn", "${data?.getStringExtra("nombreModificado")}")
+                }
+            }
+        }
+    val contenidoIntentImplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result ->
+        if(result.resultCode == RESULT_OK){
+            if(result.data != null){
+                if (result.data !!.data != null){
+                    val uri: Uri = result.data!!.data!!
+                    val cursor = contentResolver.query(
+                        uri,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                    cursor?.moveToFirst()
+                    val indiceTelefono = cursor?.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+                    val telefono = cursor?.getString(
+                        indiceTelefono!!
+                    )
+
+                    cursor?.close()
+                    Log.i("intent-epn", "Telefono ${telefono}")
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Identificador carpeta recursos
+        val botonACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        // Listener
+        botonACicloVida
+            .setOnClickListener {
+            irActividad(ACicloVida::class.java)
+        }
+
+        // Boton List View
+        val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
+        botonListView
+            .setOnClickListener {
+                irActividad((BListView::class.java))
+            }
+
+        // Intent Implicito
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                contenidoIntentImplicito.launch(intentConRespuesta)
+            }
+    }
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        // Enviar parametros (solamente varibles primitivas)
+        intentExplicito.putExtra("nombre","Adrian")
+        intentExplicito.putExtra("apellido", "Eguez")
+        intentExplicito.putExtra("edad", "33")
+        contenidoIntentExplicito.launch(intentExplicito)
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+}

26f4c48523a821635612a2e663d8ba073a6a4579
Daniela Romero <daniela.romero@epn.edu.ec>
2023-02-21 13:19:51 -0500

    Google Maps API

---
+++ 01-android/app/src/main/java/com/example/darmapplication/HGoogleMapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.darmapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HGoogleMapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hgoogle_maps2)
+    }
+}
